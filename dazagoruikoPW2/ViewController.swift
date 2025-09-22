14eb373248d821e1d8e44bb13af4f69a11394127
Daniil Zagoruyko <zagoruykodaniil@mail.ru>
2025-09-22 23:45:10 +0300

    Initial Commit

---
+++ dazagoruikoPW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dazagoruikoPW2
+//
+//  Created by Даниил on 22.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
